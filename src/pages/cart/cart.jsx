8c2c478b78439a98c2618facf7579f770d813148
luthfihanif10 <18221151@std.stei.itb.ac.id>
2023-12-07 17:34:41 +0700

    FE OnlyCommerce

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart-item";
+import { useNavigate } from "react-router-dom";
+import "./cart.css";
+
+export const Cart = () => {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      {totalAmount > 0 ? (
+        <div className="cart">
+          <div>
+            <h1>Your Cart Items</h1>
+          </div>
+          <div className="cart">
+            {PRODUCTS.map((product) => {
+              if (cartItems[product.id] !== 0) {
+                return <CartItem data={product} />;
+              }
+            })}
+          </div>
+
+          <p style={{ fontSize: "20px" }}><b> Total: Rp{totalAmount} </b></p>
+        
+          <div className="request">
+            <button onClick={() => navigate("/")}> Back </button>
+            <button
+              onClick={() => {
+                checkout();
+                navigate("/request");
+              }}
+            >
+              {" "}
+              Request{" "}
+            </button>
+          </div>
+        
+      </div>
+    ) : (
+      <h1 style={{textAlign: "center"}}> Your Cart is Empty</h1>
+    )}
+  </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.webp";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.webp";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Heavyweight Flannel Shirt",
+    price: 199000.00,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Men T-Shirt ",
+    price: 150000.00,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Women Sweater",
+    price: 299000.00,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Midwest Mallards Team Hat",
+    price: 450000.00,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Razer Wireless Gaming Mouse",
+    price: 389000.00,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Nikon Z 6II Mirrorless",
+    price: 25999999.00,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Vivo Color Earphone",
+    price: 120000.00,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Wireless Gaming Headset",
+    price: 390000.00,
+    productImage: product8,
+  },
+];
