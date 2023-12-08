181f893131b6c6b2824a7ed15cb1e156c7bf9526
Aliefnaufal <18221105@std.stei.itb.ac.id>
2023-12-08 23:11:51 +0700

    displaying produk untuk customer
    Dah aman ya ges yang buat customer

--- src/pages/customer/productCustomer.jsx
+++ src/pages/customer/productCustomer.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from "react";
+// import * as productImage from "../../assets/products";
 
 export const ProductCustomer = (props) => {
-  const { id, productName, amount, price, productImage } = props.data;
+  const { id_produk, nama_produk, jumlah_produk, harga, blok } = props.isiData;
 
   return (
     <div className="productCustomer">
-      <img src={productImage} />
+      <img src={`/src/assets/products/${id_produk}.png`} />
       <div className="description">
         <p>
-          <b>{productName}</b>
+          <b>{nama_produk}</b>
         </p>
-        <p>Stock: {amount}</p>
-        <p> Rp{price}</p>
+        <p>Posisi: {blok}</p>
+        <p> Rp{harga}</p>
       </div>
     </div>
   );
--- src/pages/customer/shopCustomer.jsx
+++ src/pages/customer/shopCustomer.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { PRODUCTS } from "../../products";
+/* eslint-disable no-unused-vars */
+import React, {useEffect, useState} from "react";
 import { ProductCustomer } from "./productCustomer";
 import { NavbarCustomer } from "../../components/navbarCustomer";
 import "./shopCustomer.css";
+import axios from "axios";
 
 export const ShopCustomer = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get('https://onlycommerce.onrender.com/v1/products')
+      .then(result => {
+        console.log('data API', result.data.data);
+
+        setProducts(result.data.data);
+      })
+      .catch(err => {
+        console.log('Error: ', err);
+      })
+  }, [])
   return (
     <div>
       <NavbarCustomer/>
@@ -14,8 +27,8 @@ export const ShopCustomer = () => {
         </div>
 
         <div className="products">
-          {PRODUCTS.map((product) => (
-            <ProductCustomer data={product} />
+          {products.map((product) => (
+            <ProductCustomer key={product.id_produk} isiData={product}/>
           ))}
         </div>
       </div>
